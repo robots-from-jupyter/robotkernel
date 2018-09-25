ae6180004ed0e3a7ec94e27c63bb0ce79f740dcb
Asko Soukka <asko.soukka@iki.fi>
2018-09-25 15:44:05 +0300

    Patch SikuliLibrary to stop remote server process more aggressively

--- pkgs/sikulixlibrary/default.nix
+++ pkgs/sikulixlibrary/default.nix
@@ -9,6 +9,7 @@ pythonPackages.buildPythonPackage rec {
     sha256 = "16s6247rq2afjsr751iriadya9jzh3390a6yc8lnd98d5h2m9zaz";
   };
   buildInputs = [ jdk sikulix ];
+  patches = [ ./stop_remote_server.patch ];
   postPatch = ''
     jar xf ${sikulix}/libexec/sikulix/sikulix.jar sikulixlibs
     jar uf target/src/SikuliLibrary/lib/SikuliLibrary.jar sikulixlibs
