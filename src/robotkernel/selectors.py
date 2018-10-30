68962995d311e2ce8b6e6f038906b8d8784b216c
Asko Soukka <asko.soukka@iki.fi>
2018-10-30 20:23:27 +0200

    Change not to add 'strText=' after AutoIT window completion

--- src/robotkernel/selectors.py
+++ src/robotkernel/selectors.py
@@ -460,7 +460,7 @@ def get_autoit_win_selector_completions(needle, driver):
     if needle:
         results = driver.WinList(needle)[0][1:]
         for result in results:
-            matches.append(f'strTitle={result}  strText=')
+            matches.append(f'strTitle={result}')
     else:
         title = driver.WinGetTitle('')
         driver.ToolTip('Select window')
@@ -469,7 +469,7 @@ def get_autoit_win_selector_completions(needle, driver):
             title_ = driver.WinGetTitle('')
             if title_ != title:
                 driver.ToolTip('')
-                matches.append(f'strTitle={title_}  strText=')
+                matches.append(f'strTitle={title_}')
                 break
     return matches
 
