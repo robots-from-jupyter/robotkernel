721fccfba3f8abe0d99e5fbec725859fcede34c4
Asko Soukka <asko.soukka@iki.fi>
2022-04-07 08:27:54 +0300

    Remove jupyterlab_robotmode to favor marketsquare/jupyterlab_robotmode

--- src/jupyterlab_robotmode/jupyterlab_robotmode/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Running `npm run build` will create static resources in the static
-# directory of this Python package (and create that directory if necessary).
-
-
-def _jupyter_labextension_paths():
-    return [{"name": "jupyterlab_robotmode", "src": "static"}]
